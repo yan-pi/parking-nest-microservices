bc6914928a9de2414c33599cb26abed2a293e18e
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-06-12 00:17:48 -0300

    refactor: delete kubernetes

--- api-gateway/src/parking-spot/dtos/entry-exit.dto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const VehicleSchema = z.object({
-  licensePlate: z.string(),
-  model: z.string(),
-  color: z.string(),
-});
-
-export type VehicleDTO = z.infer<typeof VehicleSchema>;
--- api-gateway/src/payment/dtos/payment.dto.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const CreatePaymentSchema = z.object({
-  licensePlate: z.string().nonempty('License plate cannot be empty'),
-  amount: z.number().positive('Amount must be a positive number'),
-});
-
-export type CreatePaymentDto = z.infer<typeof CreatePaymentSchema>;
