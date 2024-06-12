2b4eb359ff01ffa75734d2ce9a2a79b1622e6ca9
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-06-12 00:19:38 -0300

    refactor: parking-spot service to use rabbitmq

---
+++ parking-spot-service/src/parking-spot.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ParkingSpotService } from './parking-spot.service';
+import { ParkingSpotController } from './parking-spot.controller';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PARKING_SPOT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'parking-spot-queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ParkingSpotController],
+  providers: [ParkingSpotService, PrismaService],
+})
+export class ParkingSpotModule {}
--- parking-spot-service/src/parking-spot.service.ts
+++ parking-spot-service/src/parking-spot.service.ts
@@ -1,49 +1,25 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { InjectQueue } from '@nestjs/bull';
-import { Queue } from 'bull';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { VehicleDTO } from './dtos/parking-spot.dto';
 
 @Injectable()
-export class ParkingSpotService implements OnModuleInit {
-  constructor(
-    @InjectQueue('entry-exit-queue') private entryExitQueue: Queue,
-    private prisma: PrismaService,
-  ) {}
-
-  async onModuleInit() {
-    this.entryExitQueue.process('vehicleEntry', async (job) => {
-      const vehicleInfo: VehicleDTO = job.data;
-      // Lógica para verificar disponibilidade de vagas e alocar
-      const availableSpot = await this.prisma.parkingSpot.findFirst({
-        where: { isOccupied: false },
-      });
-      if (availableSpot) {
-        await this.prisma.parkingSpot.update({
-          where: { id: availableSpot.id },
-          data: { isOccupied: true },
-        });
-      }
-    });
-
-    this.entryExitQueue.process('vehicleExit', async (job) => {
-      const vehicleInfo: VehicleDTO = job.data;
-      // Lógica para liberar a vaga
-      const occupiedSpot = await this.prisma.parkingSpot.findFirst({
-        where: { isOccupied: true, vehicleId: vehicleInfo.id },
-      });
-      if (occupiedSpot) {
-        await this.prisma.parkingSpot.update({
-          where: { id: occupiedSpot.id },
-          data: { isOccupied: false, vehicleId: null },
-        });
-      }
-    });
-  }
+export class ParkingSpotService {
+  constructor(private prisma: PrismaService) {}
 
   async checkAvailability() {
-    return await this.prisma.parkingSpot.findMany({
+    return this.prisma.parkingSpot.findMany({ where: { isOccupied: false } });
+  }
+
+  async allocateSpot(vehicleId: number) {
+    const spot = await this.prisma.parkingSpot.findFirst({
       where: { isOccupied: false },
     });
+    if (spot) {
+      await this.prisma.parkingSpot.update({
+        where: { id: spot.id },
+        data: { isOccupied: true, vehicleId: vehicleId },
+      });
+      return spot;
+    }
+    return null;
   }
 }
