bd4f587b33023462452e713129202168391e9e44
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-06-12 20:38:10 -0300

    refactor: use rabbitmq in entry-exit service

--- entry-exit-service/src/entry-exit.service.ts
+++ entry-exit-service/src/entry-exit.service.ts
@@ -1,19 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { InjectQueue } from '@nestjs/bull';
-import { Queue } from 'bull';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { VehicleDTO } from './dtos/entry-exit.dto';
 import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class EntryExitService {
   constructor(
-    @InjectQueue('entry-exit-queue') private entryExitQueue: Queue,
+    @Inject('ENTRY_EXIT_SERVICE') private client: ClientProxy,
     private prisma: PrismaService,
   ) {}
 
   async registerEntry(vehicleInfo: VehicleDTO) {
     // Lógica para registrar a entrada do veículo
-    await this.entryExitQueue.add('vehicleEntry', vehicleInfo);
+    await this.client.emit('vehicleEntry', vehicleInfo);
     return this.prisma.vehicle.create({
       data: {
         licensePlate: vehicleInfo.licensePlate,
@@ -25,7 +24,7 @@ export class EntryExitService {
 
   async registerExit(vehicleInfo: VehicleDTO) {
     // Lógica para registrar a saída do veículo
-    await this.entryExitQueue.add('vehicleExit', vehicleInfo);
+    await this.client.emit('vehicleExit', vehicleInfo);
     return this.prisma.vehicle.update({
       where: { licensePlate: vehicleInfo.licensePlate },
       data: { exitTime: new Date() },
