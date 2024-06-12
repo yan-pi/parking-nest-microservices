274f9e0bf5eb352688a8a7d3b528cf4611be2524
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-06-12 00:19:56 -0300

    refactor: payment service to use rabbitmq

--- payment-service/src/payment.service.ts
+++ payment-service/src/payment.service.ts
@@ -1,21 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { CreatePaymentDto } from './dtos/payment.dto';
 
 @Injectable()
 export class PaymentService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-  async createPayment(createPaymentDto: CreatePaymentDto) {
-    // Verifique se createPaymentDto.vehicleId está definido
-    const { vehicleId, amount, method } = createPaymentDto;
-
-    const paymentInfo = {
-      vehicleId: vehicleId || undefined, // Pode ser undefined se não estiver definido em createPaymentDto
-      amount,
-      method,
-    };
-
-    return this.prisma.payment.create({ data: paymentInfo });
+  async processPayment(vehicleId: number, amount: number, method: string) {
+    return this.prisma.payment.create({
+      data: { vehicleId, amount, method },
+    });
   }
 }
